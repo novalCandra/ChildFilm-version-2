01b4d3c8d6bdecf6c0c898fc27deff5818f12f2f
novalCandra <dinatanovel5@gmail.com>
2026-06-28 18:55:59 +0700

    feat : 🛠️ still under repair

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import { ChevronRight } from "lucide-react"
 import LogoPng from "../assets/logo.png"
+import { useState } from "react"
 export default function Footer() {
+    const [buttonArrow_one, setButtonArrow_one] = useState(false);
+    const [buttonArrow_tow, setButtonArrow_two] = useState(false);
     return (
         <footer
             className="flex flex-col md:flex-row border border-t-gray-500 w-full px-10 py-10 justify-between space-y-4 md:space-y-0 bg-[#181A1C]">
@@ -54,17 +57,33 @@ export default function Footer() {
             <div className="flex flex-col md:hidden justify-between gap-1">
                 <div className="flex flex-row justify-between">
                     <h2 className="text-lg md:text-2xl font-lato font-bold text-white">Genre</h2>
-                    <ChevronRight className="text-white" />
+                    <ChevronRight className={`${buttonArrow_one ? "block rotate-90 text-white transition" : "block text-white rotate-0 transition"}`} onClick={() => setButtonArrow_one(prev => !prev)} />
                 </div>
-                <ul id="listGenre" className="hidden space-y-2 text-[#C1C2C4] font-bold font-lato cursor-pointer">
+                <ul className="space-y-2 text-[#C1C2C4] font-bold font-lato cursor-pointer transition-all">
+                    {buttonArrow_one && (
+                        <>
+                            <li>FAQ</li>
+                            <li>Kontak Kami</li>
+                            <li>Privasi</li>
+                            <li>Syarat & Ketentuan</li>
+                        </>
+                    )}
                 </ul>
             </div>
             <div className="flex flex-col md:hidden gap-1">
                 <div className="flex flex-row justify-between">
                     <h2 className="text-lg md:text-2xl font-lato font-bold text-white">Bantuan</h2>
-                    <ChevronRight className="text-white" />
+                    <ChevronRight className={`${buttonArrow_tow ? "block rotate-90 text-white" : "block text-white rotate-0"}`} onClick={() => setButtonArrow_two(prev => !prev)} />
                 </div>
-                <ul id="list" className="hidden space-y-2 text-[#C1C2C4] font-bold font-lato cursor-pointer">
+                <ul className="space-y-2 text-[#C1C2C4] font-bold font-lato cursor-pointer transition-all">
+                    {buttonArrow_tow && (
+                        <>
+                            <li>Aksi</li>
+                            <li>Anak-anak</li>
+                            <li>Anime</li>
+                            <li>Britania</li>
+                        </>
+                    )}
                 </ul>
             </div>
         </footer>
---
+++ src/config/ConfigFooter.js
@@ -0,0 +1,11 @@
+import LogoPng from "../assets/logo.png"
+export const ConfigFooterLandingPage = {
+    LogoGambar: LogoPng,
+    deskripsi: "&copy;2026 Chill All Right Reserved", 
+    listJudul : "Genre",
+    fieldList : [
+        {
+            
+        }
+    ]
+}
