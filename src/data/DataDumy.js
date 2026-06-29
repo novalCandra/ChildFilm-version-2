fff9d636bb5448ef2fd44bcdfd85c07b1865aa1d
novalCandra <dinatanovel5@gmail.com>
2026-06-29 11:09:17 +0700

    feat : ✅ Success Add data Dumy daftar

---
+++ src/data/DataDumy.js
@@ -0,0 +1,78 @@
+import ImageBlueVertikal from "../assets/blueVertikal.png"
+export const dataDumyDaftar = {
+    headingDaftar: "DAFTAR SAYA",
+    data: [
+        {
+            id: 1,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 2,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 3,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 4,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 5,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 6,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 1,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 2,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 3,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 4,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 5,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+        {
+            id: 6,
+            image: ImageBlueVertikal,
+            episodeBaru: "Episode Baru",
+            top: "Top 10"
+        },
+    ]
+}
