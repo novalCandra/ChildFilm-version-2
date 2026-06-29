d15828badf279a42f28a17574ebc17aa4508db39
novalCandra <dinatanovel5@gmail.com>
2026-06-29 11:19:51 +0700

    style : ✅ Success Add AOS

--- src/page/users/components/AsideComponents.jsx
+++ src/page/users/components/AsideComponents.jsx
@@ -2,7 +2,7 @@ export default function AsideComponents({ reloadPage }) {
     return (
         <aside className="grid grid-cols-3 md:grid-cols-6 px-1 py-2 md:py-2 gap-3 md:gap-4">
             {reloadPage?.map((item) => (
-                <div key={item.id} className="relative">
+                <div key={item.id} className="relative" data-aos="fade-up">
                     <span className="absolute top-3 left-1 px-1 rounded-[5px] py-1 font-lato text-md bg-[#0F1E93] text-white font-semibold text-[10px] md:text-base">{item.episodeBaru}</span>
                     <span
                         className="absolute text-white text-[0.8rem] w-6 md:w-9 md:text-base md:py-2 md:px-2 bg-[#B71F1D] right-0 md:right-12 cursor-pointer font-bold font-lato text-center rounded-bl-lg">{item.top}</span>
--- src/page/users/daftarFilm/PageDaftar.jsx
+++ src/page/users/daftarFilm/PageDaftar.jsx
@@ -1,9 +1,15 @@
 import { dataDumyDaftar } from "../../../data/DataDumy"
 import AsideComponents from "../components/AsideComponents"
+import Aos from "aos"
+import "aos/dist/aos.css"
 export default function PageDaftar() {
+    Aos.init({
+        duration: 1200,
+        delay: 1000
+    })
     return (
         <main className="bg-[#22282A] min-h-screen">
-            <section className="px-3 pt-10 md:p-20 md:py-30 space-y-3.5 w-full">
+            <section className="px-3 pt-10 md:p-20 md:py-10 space-y-3.5 w-full">
                 <h1 className="font-lato text-white font-bold text-xl md:text-3xl">DAFTAR SAYA</h1>
                 <AsideComponents reloadPage={dataDumyDaftar.data} />
             </section>
