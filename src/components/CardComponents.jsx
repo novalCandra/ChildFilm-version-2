3dbac0e330733a6a65652acc01bb46b9618465bd
novalCandra <dinatanovel5@gmail.com>
2026-07-01 09:51:08 +0700

    feat : ✅ Perbaikan Font-lato

--- src/components/CardComponents.jsx
+++ src/components/CardComponents.jsx
@@ -14,14 +14,14 @@ export default function CardComponents({ title, deskripsi, deskripsiText, linkTw
                 <img src={imageLogo} alt="Foto Logo" className="w-32 items-center justify-center" />
             </div>
             <div className="space-y-3 md:space-y-3">
-                <h2 className="text-xl md:text-3xl text-white text-center font-bold">{title}</h2>
-                <p className="text-md md:text-md text-white text-center">{deskripsi}</p>
+                <h2 className="text-xl md:text-3xl text-white text-center font-bold font-lato">{title}</h2>
+                <p className="text-md md:text-md text-white text-center font-lato">{deskripsi}</p>
             </div>
             <form onSubmit={handleSubmit(onSubmit)} className="-mt-5 space-y-2 md:px-5 w-85 md:w-600 max-w-md mx-auto">
                 {formFiled?.map((field) => (
                     <>
                         <div className="flex flex-col">
-                            <label key={field.id} htmlFor={field.name} className="text-white text-md  md:text-lg">{field.label}</label>
+                            <label key={field.id} htmlFor={field.name} className="text-white text-md  md:text-lg font-lato">{field.label}</label>
                         </div>
                         <input id={field.name} type={field.type} name={field.name} placeholder={field.placehonder}
                             className="w-full rounded-2xl border-2 border-white px-3 py-2 text-white" {...register(field.name, { required: true })} />
@@ -29,14 +29,14 @@ export default function CardComponents({ title, deskripsi, deskripsiText, linkTw
                     </>
                 ))}
                 <div className="flex flex-row justify-between gap-4">
-                    <p className="text-gray-400 text-sm md:text-md">{deskripsiText} <Link
-                        to={linkPath} className="text-white text-sm md:text-md">{link}</Link></p>
-                    <Link href="link" className="text-white text-sm md:text-md">{linkTwo}</Link>
+                    <p className="text-gray-400 text-sm md:text-md font-lato">{deskripsiText} <Link
+                        to={linkPath} className="text-white text-sm md:text-md font-lato">{link}</Link></p>
+                    <Link href="link" className="text-white text-sm md:text-md font-lato">{linkTwo}</Link>
                 </div>
                 <div className="flex flex-col space-y-2">
                     <button type="submit"
-                        className="px-3 py-3 bg-[#3D4142] rounded-4xl text-white cursor-pointer  border border-gray-500">{textButton}</button>
-                    <p className="text-white text-center">OR</p>
+                        className="px-3 py-3 bg-[#3D4142] rounded-4xl text-white cursor-pointer  border border-gray-500 font-lato">{textButton}</button>
+                    <p className="text-white text-center font-lato">OR</p>
                     <Link to="google.com"
                         className="flex flex-row gap-3 px-3 py-3 bg-transparent rounded-4xl text-white border border-gray-500 cursor-pointer justify-center items-center">
                         <img src={imageLogoGoogle} width="20" alt="button" />{textButtonGoogle}
