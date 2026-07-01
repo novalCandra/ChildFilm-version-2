fb85dca4d3d5be8cb259ce8f994c4d04972ef522
novalCandra <dinatanovel5@gmail.com>
2026-07-01 09:48:01 +0700

    feat : ✅ Add Sucess Baground Image (Auth)

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -4,7 +4,7 @@ import { ConfigAuthLogin } from "./config/LoginConfig";
 export default function LoginPage() {
     return (
         <>
-            <div className={`absolute inset-0 bg-[url(public/bagroundLogin.jpg)] bg-center bg-repeat bg-cover`} />
+            <div className={`absolute inset-0 bg-[url(assets/bagroundLogin.jpg)] bg-center bg-repeat bg-cover`} />
             <div className="relative flex min-h-screen items-center justify-center">
                 <CardComponents title={ConfigAuthLogin.title} imageLogo={ConfigAuthLogin.imageLogo} imageLogoGoogle={ConfigAuthLogin.imageLogoGoogle} deskripsi={ConfigAuthLogin.deskripsi} deskripsiText={ConfigAuthLogin.deskripsiText} textButton={ConfigAuthLogin.textButton} link={ConfigAuthLogin.link} linkPath={ConfigAuthLogin.linkPath} textButtonGoogle={"Masuk dengan Google"} schema={SchemaLogin} formFiled={ConfigAuthLogin.fields} linkTwo={ConfigAuthLogin.linkTwo} />
             </div>
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -5,7 +5,7 @@ import { ConfigRegister } from './config/RegisterConfig'
 export default function RegisterPage() {
     return (
         <>
-            <div className={`absolute inset-0 bg-[url(public/bagroundRegister.jpg)] bg-center bg-repeat bg-cover`} />
+            <div className={`absolute inset-0 bg-[url(assets/bagroundRegister.jpg)] bg-center bg-repeat bg-cover`} />
             <div className="relative flex min-h-screen items-center justify-center">
                 <CardComponents title={ConfigRegister.title} imageLogo={ConfigRegister.imageLogo} imageLogoGoogle={ConfigRegister.imageLogoGoogle} deskripsi={ConfigRegister.deskripsi} deskripsiText={ConfigRegister.deskripsiText} textButton={ConfigRegister.textButton} link={ConfigRegister.link} textButtonGoogle={ConfigRegister.textButtonGoogle} formFiled={ConfigRegister.fields} schema={SchemaRegister} linkPath={ConfigRegister.linkPath} />
             </div>
